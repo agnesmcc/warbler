dbb938b927c4b39af63284548a688ac15607470c
Agnes <agnes.mccall5@gmail.com>
2024-06-18 21:43:12 -0400

    test that user can delete a message

--- test_message_views.py
+++ test_message_views.py
@@ -52,7 +52,7 @@ class MessageViewTestCase(TestCase):
         db.session.commit()
 
     def test_add_message(self):
-        """Can use add a message?"""
+        """Can user add a message?"""
 
         # Since we need to change the session to mimic logging in,
         # we need to use the changing-session trick:
@@ -71,3 +71,24 @@ class MessageViewTestCase(TestCase):
 
             msg = Message.query.one()
             self.assertEqual(msg.text, "Hello")
+
+    def test_delete_message(self):
+        """Can user delete a message?"""
+
+        m = Message(text="Test message", user_id=self.testuser.id)
+        db.session.add(m)
+        db.session.commit()
+
+        msg_id = m.id # To avoid DetachedInstanceError http://sqlalche.me/e/bhk3
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            resp = c.post(f"/messages/{msg_id}/delete")
+
+            self.assertEqual(resp.status_code, 302)
+
+            m = Message.query.get(m.id)
+            self.assertIsNone(m)
+
